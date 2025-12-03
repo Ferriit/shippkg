4eb900c6ab39fec30aaaf6eefbd81951ad91e81b
Ferriit <ferriitalt@gmail.com>
2025-12-03 19:25:22 +0100

    added proper parsing, writing and added logging with customization

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,68 @@ import java.io.File
 import java.net.URL
 
 
+const val ROOT_PATH = "/etc/ship"
+const val SHIP_FILE = "/etc/ship/ship.pkg"
+const val debug = true
+
+// Customizable things done by fetching from the ship.pkg file
+var messageEnd = ""
+var errorPrefix = ""
+var warningPrefix = ""
+var infoPrefix = ""
+var inputPrefix = ""
+
+var suffixColor = ""
+var errorColor = ""
+var warningColor = ""
+var infoColor = ""
+var inputColor = ""
+
+
+enum class LogType {
+    INFO,
+    WARN,
+    ERR,
+    INPUT,
+    DBG
+}
+
+
+fun log(type: LogType, Message: String) {
+    val colorTable = mapOf(
+        "default" to "\u001B[1m",
+        "black"   to "\u001B[1;30m",
+        "red"     to "\u001B[1;31m",
+        "green"   to "\u001B[1;32m",
+        "yellow"  to "\u001B[1;33m",
+        "blue"    to "\u001B[1;34m",
+        "purple"  to "\u001B[1;35m",
+        "cyan"    to "\u001B[1;36m",
+        "white"   to "\u001B[1;37m",
+        "reset"   to "\u001B[0m"
+    )
+
+
+    val prefix = when (type) {
+        LogType.INFO -> infoPrefix
+        LogType.WARN -> warningPrefix
+        LogType.ERR -> errorPrefix
+        LogType.INPUT -> inputPrefix
+        LogType.DBG -> "[DEBUG] "
+    }
+    
+    val color = when (type) {
+        LogType.INFO -> colorTable[infoColor] ?: ""
+        LogType.WARN -> colorTable[warningColor] ?: ""
+        LogType.ERR -> colorTable[errorColor] ?: ""
+        LogType.INPUT -> colorTable[inputColor] ?: ""
+        LogType.DBG -> colorTable["purple"] ?: ""
+    }
+
+    println("$color$prefix:\u001B[0m $Message $messageEnd")
+}
+
+
 fun stripComments(source: String): String {
     val commentRegex = Regex("""(?m)#.*?$""")
     val NoComments: String = source.replace(commentRegex, "")
@@ -24,9 +86,10 @@ fun stripComments(source: String): String {
     return Output
 }
 
-fun ParseTOML(data: String): MutableMap<String, MutableMap<String, String>> {
+
+fun parseTOML(data: String): MutableMap<String, MutableMap<String, String>> {
     val toml: MutableMap<String, MutableMap<String, String>> = mutableMapOf()
-    val noComments = stripComments(data)
+    val noComments = stripComments(data).replace("\"", "").replace("'", "")
 
     var currentSection = "" // currently active section
 
@@ -55,6 +118,17 @@ fun ParseTOML(data: String): MutableMap<String, MutableMap<String, String>> {
 }
 
 
+fun writeTOML(data: Map<String, Map<String, String>>): String =
+    buildString {
+        for ((section, values) in data) {
+            appendLine("[$section]")
+            for ((key, value) in values) {
+                appendLine("$key = \"$value\"")
+            }
+            appendLine()  // blank line after each section
+        }
+    }
+
 
 fun DownloadFile(url: String, OutputPath: String) {
     URL(url).openStream().use { input ->
@@ -64,8 +138,24 @@ fun DownloadFile(url: String, OutputPath: String) {
     }
 }
 
-fun main() {
-    //DownloadFile("https://ferriit.gregtech.eu/kingjamesbible.txt", "/home/ferriit/shippkg/bible.txt");
-    var InputFile = File("tests/TOML_test.toml")
-    println(ParseTOML(InputFile.readText())["Servers"]?.get("Server1"))
+
+fun main(args: Array<String>) {
+    var shipPKGFile = File(SHIP_FILE)
+
+    val shipPKG = parseTOML(shipPKGFile.readText())
+
+    // Get customization data from ship.pkg
+    val customizeText = shipPKG["Customize.text"] ?: mutableMapOf()
+    messageEnd = customizeText["LoggingEnd"] ?: ""
+    errorPrefix = customizeText["ErrorPrefix"] ?: "E"
+    warningPrefix = customizeText["WarningPrefix"] ?: "W"
+    infoPrefix = customizeText["InfoPrefix"] ?: "I"
+    inputPrefix = customizeText["InputPrefix"] ?: "?"
+
+    val customizeColor = shipPKG["Customize.colors"] ?: mutableMapOf()
+    suffixColor = customizeColor["LoggingEndColor"] ?: ""
+    errorColor = customizeColor["ErrorPrefixColor"] ?: "red"
+    warningColor = customizeColor["WarningPrefixColor"] ?: "yellow"
+    infoColor = customizeColor["InfoPrefixColor"] ?: "blue"
+    inputColor = customizeColor["InputPrefixColor"] ?: "white"
 }
